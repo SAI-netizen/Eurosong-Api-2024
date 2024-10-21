e4a650e226403f57dc62648d6410359c9adb31b8
Soumya AI <soumya.a.i@hotmail.com>
2024-10-21 12:30:14 +0200

    added api/artitst

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ app.get('/', (req, res) => {
     res.send('Hello World'); 
 }); 
 
+
+app.get('/api/artists', (req, res) => {
+    res.send(
+        [
+            "JB",
+            "Beyonce",
+        ]
+    ); 
+}); 
+
 // Starten server en op welke port moet server luisteren 
 app.listen(3000, () => {
     console.log('Server is running op port 3000'); 
